03899bca882345271715cfc3d453fe9926d770e2
Tomislav Markovski <tomislav.markovski@dealogic.com>
2016-12-01 19:44:35 +0100

    Initial Commit

---
+++ SwiftBridgeCommander/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftBridgeCommander
+//
+//  Created by Markovski, Tomislav on 12/1/16.
+//  Copyright © 2016 Blue Metal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
