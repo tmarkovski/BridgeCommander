ce7bb3219152c3cb29a25c8a55f53da5ef7eaa17
Tomislav Markovski <tomislav.markovski@dealogic.com>
2016-12-02 12:02:27 +0100

    Framework cleanup

---
+++ BridgeCommander/BridgeCommander.h
@@ -0,0 +1,19 @@
+//
+//  BridgeCommander.h
+//  BridgeCommander
+//
+//  Created by Markovski, Tomislav on 12/2/16.
+//  Copyright © 2016 Blue Metal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BridgeCommander.
+FOUNDATION_EXPORT double BridgeCommanderVersionNumber;
+
+//! Project version string for BridgeCommander.
+FOUNDATION_EXPORT const unsigned char BridgeCommanderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BridgeCommander/PublicHeader.h>
+
+
